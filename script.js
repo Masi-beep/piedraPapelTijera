74f8bb28a92743ea30f7ab53f86faec31e6e47d3
masi-beep <maximo.lizarraga@gmail.com>
2022-02-26 00:12:28 -0300

    agregué el sistema de puntos

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// GAME
+let playerScore = 0;
+let computerScore = 0;
+let winnerRound = "";
+
 function computerPlay() {
     let eleccion = Math.floor(Math.random()* 3) + 1;
 
@@ -14,18 +19,41 @@ function playRound(playerSelection,computerSelection) {
     let pS = playerSelection.toLowerCase();
     let cS = computerSelection.toLowerCase();
 
-    if ((pS == "piedra" && cS == "tijera") ||
-        (pS == "tijera" && cS == "papel") ||
-        (pS == "papel" && cS == "piedra")) {
+    if ((pS === "piedra" && cS === "tijera") ||
+        (pS === "tijera" && cS === "papel") ||
+        (pS === "papel" && cS === "piedra")) {
+        playerScore++;
         return `Ganaste, ${pS} le gana a ${cS}`;
-    } else if (pS == cS) {
+    } else if (pS === cS) {
         return `Empate, ambos eligieron ${pS}`;
     } else {
+        computerScore++;
         return `Perdiste, ${cS} le gana a ${pS}`;
     }   
 }
 
+function game() {
+    let roundNumber = 0;
+    for (let i = 0; i < 5; i++){
+        let playerChoice = prompt("Piedra, Papel o Tijera"); 
+        let computerChoice = computerPlay();
+        roundNumber++;
+        console.log(`Ronda ${roundNumber}: `);
+        console.log(playRound(playerChoice,computerChoice));
+        
+        if ((playerScore === 3 && roundNumber === 3) || (playerScore > computerScore && roundNumber === 5)) {
+            console.log("Ganaste el juego! F5 para reiniciar");
+            break;
+        } else if ((computerScore === 3 && roundNumber === 3) || (computerScore > playerScore && roundNumber === 5)) {
+            console.log("Perdiste el juego... F5 para reiniciar");
+            break;
+        } else if (computerScore === playerScore && roundNumber === 5) {
+            console.log("Empate. F5 para reiniciar");
+        }
+    }
+}
 
-let playerSelection = "piedra";
-let computerSelection = computerPlay();
-console.log(playRound(playerSelection,computerSelection));
+game();
+//let playerSelection = "piedra";
+//let computerSelection = computerPlay();
+//console.log(playRound(playerSelection,computerSelection));
